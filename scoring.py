1209b8f8fd627dc367332b55a1f7891a91cc6d0a
dandy999-ai <danilonapoli099@gmail.com>
2026-08-04 17:38:21 +0200

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,17 +1,19 @@
 """
-TradingBot PRO V2.1
+TradingBot PRO V3.2
 Sistema di Punteggio
 """
 
 from config import (
     TECHNICAL_WEIGHT,
-    FUNDAMENTAL_WEIGHT
+    FUNDAMENTAL_WEIGHT,
+    MOMENTUM_WEIGHT
 )
 
 
 def total_score(
     technical_score,
-    fundamental_score
+    fundamental_score,
+    momentum_score
 ):
     """
     Calcola il punteggio finale.
@@ -19,7 +21,8 @@ def total_score(
 
     score = (
         technical_score * TECHNICAL_WEIGHT +
-        fundamental_score * FUNDAMENTAL_WEIGHT
+        fundamental_score * FUNDAMENTAL_WEIGHT +
+        momentum_score * MOMENTUM_WEIGHT
     )
 
     return round(min(score, 100), 2)
